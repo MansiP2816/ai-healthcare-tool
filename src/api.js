0c230b67e204580f099be89d67a5d79a85b77dc2
MansiP2816 <mansipurohit802@gmail.com>
2025-08-29 21:26:26 +0530

    Create api.js

---
+++ src/api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+const API = axios.create({ baseURL: "http://127.0.0.1:8000" });
+
+export const analyzeData = (file) => {
+  const formData = new FormData();
+  formData.append("file", file);
+  return API.post("/analyze/", formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+};
